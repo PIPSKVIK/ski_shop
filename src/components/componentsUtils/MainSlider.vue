67cf66f511ed5e87f324cbcccb7ff73a01c641bf
Alexander <milovanovska@yandex.ru>
2021-03-01 00:28:04 +0300

    Add component for main slider

---
+++ src/components/componentsUtils/MainSlider.vue
@@ -0,0 +1,67 @@
+<template>
+  <swiper class="swiper" :options="swiperOption">
+    <swiper-slide><span>pickture_1</span></swiper-slide>
+    <swiper-slide><span>pickture_2</span></swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+    <div class="swiper-button-prev" slot="button-prev"></div>
+    <div class="swiper-button-next" slot="button-next"></div>
+  </swiper>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
+
+export default {
+  name: "MainSlider",
+  title: "Loop mode / Infinite loop",
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  data() {
+    return {
+      swiperOption: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        }
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.swiper {
+  height: 500px;
+
+  .swiper-slide {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-weight: bold;
+    font-size: 18px * 2;
+    background-color: white;
+  }
+}
+.swiper-button-next::after,
+.swiper-button-prev::after {
+  color: var(--vue-color);
+}
+.swiper-pagination-bullet .swiper-pagination-bullet-active {
+  color: var(--swiper-theme-color);
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="home-page">
     <div class="home-page__main-slider">
-      <swiper class="swiper" :options="swiperOption">
-        <swiper-slide><span>pickture_1</span></swiper-slide>
-        <swiper-slide><span>pickture_2</span></swiper-slide>
-        <div class="swiper-pagination" slot="pagination"></div>
-        <div class="swiper-button-prev" slot="button-prev"></div>
-        <div class="swiper-button-next" slot="button-next"></div>
-      </swiper>
+      <main-slider />
     </div>
     <div class="home-page__advantages">
       <home-page-advantages>
@@ -20,42 +14,19 @@
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from "vue-awesome-swiper";
 import "swiper/css/swiper.css";
 import HomePageAdvantages from "@/layouts/homePageLayout/HomePageAdvantages";
 import AppIcon from "@/components/appComponents/AppIcon";
 import IconQuality from "@/assets/icons/icon-quality";
+import MainSlider from "@/components/componentsUtils/MainSlider";
 
 export default {
   name: "HomePage",
-  title: "Loop mode / Infinite loop",
   components: {
+    MainSlider,
     IconQuality,
     AppIcon,
-    HomePageAdvantages,
-    Swiper,
-    SwiperSlide
-  },
-  data() {
-    return {
-      swiperOption: {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        loop: true,
-        autoplay: {
-          delay: 3000,
-          disableOnInteraction: false
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev"
-        }
-      }
-    };
+    HomePageAdvantages
   }
 };
 </script>
@@ -68,25 +39,5 @@ export default {
   &__advantages {
     padding-top: 10px;
   }
-}
-.swiper {
-  height: 500px;
-
-  .swiper-slide {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    font-weight: bold;
-    font-size: 18px * 2;
-    background-color: white;
-  }
-}
-.swiper-button-next::after,
-.swiper-button-prev::after {
-  color: var(--vue-color);
-}
-.swiper-pagination-bullet .swiper-pagination-bullet-active {
-  color: var(--swiper-theme-color);
 }
 </style>
